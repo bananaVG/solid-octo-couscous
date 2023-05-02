de8495f0fde326be521e73e562d0dfae3e29e8a0
bananaVG <78326537+bananaVG@users.noreply.github.com>
2023-05-01 19:09:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Peaches=""
+Vadivelu=""
+
+function preload(){
+    Peaches=loadSound("peaches.mp3");
+    Vadivelu=loadSound("vadivelu.mp3");
+}
+
+function setup(){
+    createCanvas();
+    canvas.center();
+}
+
+extra.hide();
+
+
+function draw(){
+    
+}
+
